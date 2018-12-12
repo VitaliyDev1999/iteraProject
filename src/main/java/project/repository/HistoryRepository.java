d4fea1a90b0d3a680dfdfcbfde600b8ccb1dac90
opakhovs <opahovskiy@gmail.com>
2018-12-13 01:26:51 +0200

    GetHistory1.0

--- src/main/java/project/repository/HistoryRepository.java
+++ src/main/java/project/repository/HistoryRepository.java
@@ -13,5 +13,4 @@ public interface HistoryRepository extends JpaRepository<HistoryDbEntity, Long> 
     @Query(value = "SELECT s FROM history s where s.ip_address_id = :idIpEntity ORDER BY s.id desc LIMIT 5",
             nativeQuery = true)
     List<HistoryDbEntity> findAllByIpEqualsOrderById(@Param("idIpEntity") IdIpEntity idIpEntity);
-
 }
