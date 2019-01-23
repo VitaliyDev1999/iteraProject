ec674fe08c48e4e941ce8bd735b90524f03ae642
Mirosha <miroslav.boiko228@gmail.com>
2019-01-23 20:22:19 +0200

    Returned some of functional

--- src/main/java/project/service/StatisticService.java
+++ src/main/java/project/service/StatisticService.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface StatisticService {
     List<Statistic> getStatistic(String ipAddress);
+
+    List<Statistic> getStatistic(RangeStringEntity request, String ipAddress);
 }
